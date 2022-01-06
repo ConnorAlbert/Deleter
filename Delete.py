aa2dfab508b634d0d3f20a0fa53fc70ccf393014
youhungry <92011318+youhungry@users.noreply.github.com>
2022-01-05 19:38:14 -0600

    Made several changes
    Added the ability to delete both folders and files. Added exceptions for errors and program will now close when directory is empty.

--- Delete.py
+++ Delete.py
@@ -1,35 +1,51 @@
 import os
 import shutil
 
-def Delete_Files():
-    os.chdir(r"Your directory here")  # Current location
-    directory = r"Your directory here"
-    for filename in os.listdir(directory):  # list files in directory
-        f = os.path.join(directory, filename)
-        if os.path.isfile(f):
-            delete = filename
-            print(f)
+while True:
+    directory = r"Your directory here"  # Current location
+    dst = r"Directory to move to here"  # Move to location
+    os.chdir(str(directory))
+    dir = os.listdir(str(directory))
 
-    src = f  # Move from current location
-    dst = r"Directory to move file"  # Move to location
+    # Checking if the list is empty or not
+    if len(dir) == 0:
+        print("Directory is empty goodbye")
+        exit()
 
-    print("Press Enter to delete and n to move: ")
-    file = input(f'Do you want to delete:{delete} ')
+    print("\nlist of files/folders")
+    print("------------------")
+    for myfiles in os.listdir(directory):  # list files in directory
+        File_Folder = os.path.join(myfiles)
+        if os.path.isdir(File_Folder) or os.path.isfile(File_Folder):
+            print(File_Folder)
+
+    move = File_Folder
+
+    print("------------------")
+    print("\nPress Enter to delete and m to move: ")
+    file = input(f'Do you want to delete: {File_Folder} ??? ')
     if file == '':
         print('\nStarting the removal of the file !')
-        os.remove(delete)
-        print('\nFile, ', delete,
+        try:
+            os.remove(File_Folder)
+        except PermissionError:
+            shutil.rmtree(File_Folder)
+        print('\nFile, ', File_Folder,
               'The file deletion is successfully completed !!\n')
-        Delete_Files()
+
     elif file == "m":
-        if os.path.isdir(dst+'/'+delete):
-            print(delete, 'exists in the destination path!')
-            shutil.rmtree(dst+'/'+delete)
-        elif os.path.isfile(dst+'/'+delete):
-            os.remove(dst+'/'+delete)
-            print(delete, 'deleted in', dst)
-        dest = shutil.move(src, dst)
-    Delete_Files()
+        try:
+            dest = shutil.move(move, dst)
+        except shutil.Error as err:
+            if os.path.isdir(dst+'/'+File_Folder):
+                print(File_Folder, 'exists in the destination path!')
+                shutil.rmtree(dst+'/'+File_Folder)
+                print(File_Folder, 'deleted in', dst)
+            elif os.path.isfile(dst+'/'+File_Folder):
+                print(File_Folder, 'exists in the destination path!')
+                os.remove(dst+'/'+File_Folder)
+                print(File_Folder, 'deleted in', dst)
+            dest = shutil.move(move, dst)
 
-
-Delete_Files()
+    else:
+        exit()
